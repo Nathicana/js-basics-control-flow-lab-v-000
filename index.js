d2b5a7912f278b206d40be49f115db4d59a2faf8
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-19 03:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet){
-  let result 
+  let result
   if (feet <= 400){
   result = 'This one is on me!';
 }
-return result 
+if (feet > 2000){
+  result = 'I will gladly take your thirty bucks.'
 }
+return result
+}
+
